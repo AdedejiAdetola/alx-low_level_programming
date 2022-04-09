8fe742a172270aa68fe16e226429dc3f34d17776
Adetola Adedeji <adetolaadedeji0@gmail.com>
2022-04-08 21:28:12 -0700

    comb5 settled

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+int n1 = 48;
+int a = 0;
+int b;
+int comm = 44;
+
+while (a <= 99)
+{
+putchar((a / 10) + n1);
+putchar((a % 10) + n1);
+putchar(32);
+putchar((b / 10) + n1);
+putchar((b % 10) + n1);
+if (a != 98 || b != 99)
+{
+putchar(com);
+putchar(32);
+}
+b += 1;
+}
+a += 1;
+}
+putchar('\n');
+return (0);
+}
