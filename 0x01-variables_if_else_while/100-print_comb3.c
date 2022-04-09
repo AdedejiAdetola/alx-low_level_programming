db687e651834cb6639a0a4007a847bb64c21c585
Adetola Adedeji <adetolaadedeji0@gmail.com>
2022-04-08 21:02:44 -0700

    Extras

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+int j1 = 48;
+int j2;
+int comm = 44;
+while (j1 <= 57)
+{
+j2 = j1 + 1;
+while (j2 <= 57)
+{
+putchar(j1);
+putchar(j2);
+if (j1 != 56 || j2 != 57)
+{
+putchar(comm);
+putchar(32);
+}
+j2 += 1;
+}
+putchar('\n');
+return (0);
+}
