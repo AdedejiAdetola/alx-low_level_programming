c07d54d6dd147fbce4715a3dbdbcc9870c8e6683
Adetola Adedeji <adetolaadedeji0@gmail.com>
2022-04-08 20:42:10 -0700

    Hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+int j = 48;
+int k = 'a';
+while (j <= 57)
+{
+putchar(j);
+j += 1;
+}
+while (k <= 'f')
+{
+putchar(k);
+k += 1;
+}
+putchar('\n');
+return (0);
+}
