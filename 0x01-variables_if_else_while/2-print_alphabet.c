027cb4f6a6629138d2e7006edaf5178626acb188
Adetola Adedeji <adetolaadedeji0@gmail.com>
2022-04-08 19:46:04 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Print all alphabets in lower case
+ * Return: 0
+ */
+int main(void)
+{
+for (i = 'A'; i <= 'Z'; i++)
+{
+putchar(tolower(i));
+putchar(tolower('\n');
+}
+return (0);
+}
