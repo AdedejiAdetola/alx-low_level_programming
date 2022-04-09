7dd95b21fb1e15538f9edd8beb7e0df40067d94d
Adetola Adedeji <adetolaadedeji0@gmail.com>
2022-04-08 20:52:50 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+int j = 48;
+int comm = 44;
+while (n <= 57)
+{
+putchar(j);
+if (j != 57)
+{
+putchar(comm);
+putchar(32);
+}
+j += 1;
+}
+putchar('\n');
+return (0);
+}
