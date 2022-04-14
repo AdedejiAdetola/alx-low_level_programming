e8d1472c97c2d79ddd5a969c7d0043322376ea28
Adetola Adedeji <adetolaadedeji0@gmail.com>
2022-04-14 15:31:04 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#inclue "main.h"
+
+/**
+ * print_line - draw line 
+ * @n: input value
+ * Return: void
+ */
+void print_line(void)
+{
+int i = 0;
+while (i < n)
+{
+if (n > 0)
+{
+_putchar(95);
+i += 1;
+}
+else
+_putchar('\n');
+}
+_putchar('\n');
+}
