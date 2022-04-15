a3d2a7bb507a14c3db5c421e1c997ea6bb7e68f3
Adetola Adedeji <adetolaadedeji0@gmail.com>
2022-04-15 04:17:00 -0700

    another day to try hard

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * print_line - draw line 
- * @n: input value
- * Return: void
+ * print_line - a function that draws a straight line in the terminal.
+ * followed by a new line.
+ * @n: An input integer
+ * Return: Always 0
  */
+
 void print_line(int n)
 {
 int i = 0;
