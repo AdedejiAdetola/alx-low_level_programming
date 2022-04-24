0fe81dd7251b943d6c5a6266d8ab44b1e970ccee
Adetola Adedeji <adetolaadedeji0@gmail.com>
2022-04-24 06:11:53 -0700

    Stringg

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings,
+ * @dest: destination
+ * @src: source
+ * @n: amt of bytes
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int count = 0; count2 = 0;
+while (*(dest + count) != '\0')
+{
+*(dest + count) = *(src + count2);
+if (*(src + count2) == '\0')
+{
+break;
+}
+count++;
+count2++;
+}
+return (dest);
+}
