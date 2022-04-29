8a37b8cbcf397ba18a12f4bd18f0a5ede1cc682b
Adetola Adedeji <adetolaadedeji0@gmail.com>
2022-04-28 21:16:52 -0700

    Square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - return natural square root of number
+ * @n: input number
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+if (n == 0)
+{
+return (0);
+}
+if (n == 1)
+{
+return (1);
+}
+if (n < 0)
+{
+return (-1);
+}
+return (getNumSqr(n, 1));
+}
+
+/**
+ * getNumSqrv- get num of sqrt
+ * @num: input num'
+ * @i: counter
+ * Return: int
+ */
+
+int getNumSqr(int num, int i)
+{
+if (num == (i * i))
+{
+return (i);
+}
+else if (num > (i * i))
+{
+return (getNumSqr(num, i + 1));
+}
+else
+{
+return (-1);
+}
+}
