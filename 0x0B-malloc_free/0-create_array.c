743998eec96b33b5862ff45afcb64e73e756b8f6
Adetola Adedeji <adetolaadedeji0@gmail.com>
2022-05-05 14:18:52 -0700

    First soln

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+/**
+ * create_array - create array of characters
+ * @size: size
+ * @c: input val
+ * Return: char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *ar;
+unsigned int i = 0;
+
+if (size != 0 )
+{
+ar = malloc(size * sizeof(char));
+}
+else
+return (NULL);
+
+if (ar == NULL)
+return (NULL);
+
+while (i < size)
+{
+ar[i] = c;
+i += 1;
+}
+return (ar);
+}
