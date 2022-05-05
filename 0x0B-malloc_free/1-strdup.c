c348be370b73c86bc6442f6400686ec680908095
Adetola Adedeji <adetolaadedeji0@gmail.com>
2022-05-05 14:51:27 -0700

    Str duplicated

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - return pointer to new space ein memory
+ * @str: input char
+ * Return: char
+ *
+ */
+
+char *_strdup(char *str)
+{
+char *dup;
+unsigned int i = 0;
+unsigned int j = 0;
+
+if (str == NULL)
+return (NULL);
+
+while (str[i] != '\0')
+i += 1;
+
+i += 1;
+dup = malloc(i * sizeof(*dup));
+if (dup == NULL)
+return (NULL);
+
+while (j < i)
+{
+dup[j] = str[j];
+j += 1;
+}
+return (dup);
+}
