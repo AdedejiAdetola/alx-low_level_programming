a4cdc0a0b4bcca6df142e03e34bc26444f81cee9
unknown <adetolaadedeji0@gmail.com>
2022-05-10 01:35:37 +0100

    2-call0c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
+
 /**
  * malloc_checked - Entry Point
  * @b: input amount
  * Return: pointer to new mem
  */
+ 
 void *malloc_checked(unsigned int b)
 {
 void *n;
