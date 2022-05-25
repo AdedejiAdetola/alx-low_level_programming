3e53a5ff43b90b74258c5bc902f2c8e46a6bb462
Adetola Adedeji <adetolaadedeji0@gmail.com>
2022-05-25 02:58:39 +0100

    ADD node 1

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,8 +1,7 @@
-i#include <stdlib.h>
 #include <stdio.h>
-#include "lists.h"
+#include <stdlib.h>
 #include <string.h>
-
+#include "lists.h"
 /**
  * add_node - adds a new node at the beginning
  * of a list_t list.
